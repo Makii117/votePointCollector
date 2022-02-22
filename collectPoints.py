1d29ab0e7225a8c211f59c37280a54ae1c569d81
Makii117 <MakiOnPc@gmail.com>
2022-02-22 23:59:46 +0100

    Added reading login info from file(in case of multiple accounts)

--- collectPoints.py
+++ collectPoints.py
@@ -26,9 +26,7 @@ options.add_argument("--window-size=1100,1000")
 #options.add_argument('--headless')
 
 #load buster captcha solver extension into the driver
-#TODO: 
-#     Get packaged extension for the driver so it comes with the app
-options.add_argument("--load-extension=C:\\Users\\BIOSs\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions\\mpbjkejclgfgadiemmefgebjfooflfhl\\1.3.1_0")
+options.add_extension("./Buster.crx")
 
 
 driver = webdriver.Chrome(executable_path = path,chrome_options=options)
@@ -38,40 +36,52 @@ url="https://www.warmane.com/account/login"
 username = ""
 password = ""
 
+
+
 try:
-    driver.get(url)
-    time.sleep(1)
-    unameElement=driver.find_element_by_id("userID")
-    passwordElement=driver.find_element_by_id("userPW")
-    time.sleep(1)
-    unameElement.click
-    time.sleep(2)
-    unameElement.send_keys(username)
-    time.sleep(2)
-    passwordElement.click()
-    time.sleep(2)
-    passwordElement.send_keys(password)
-    time.sleep(2)
-    #load captcha iframe
-    driver.switch_to_frame(driver.find_element_by_xpath("/html/body/div[3]/div[5]/div/div[2]/div[1]/form/table/tbody/tr[7]/td/div/div/div/iframe"))
-    #click captcha
-    time.sleep(2)
-    captchaClick=driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[1]/div/div/span/div[1]')
-    time.sleep(1)
-    captchaClick.click()
-    time.sleep(random.randrange(3,6))
-    #handle new iframe popup
-    driver.switch_to_default_content()
-    driver.switch_to_frame(driver.find_element_by_xpath("/html/body/div[5]/div[4]/iframe"))
-    time.sleep(5)
-    busterButton = driver.find_element_by_xpath('/html/body/div/div/div[3]/div[2]/div[1]/div[1]/div[4]')
-    busterButton.click()
-    time.sleep(10)
-    #leave captcha iframe
-    driver.switch_to_default_content()
-    loginButton = driver.find_element_by_xpath('//*[@id="frmLogin"]/div/button')
-    loginButton.click()
-      
+    #get login info from the file (in case of multiple accounts)
+    with open('login.txt') as credentials:
+        lines = credentials.readlines()[1:]
+    for line in lines:    
+        creds = line.split(",")  
+        username = creds[0]
+        password = creds[1].lstrip()  
+        
+        driver.get(url)
+        time.sleep(1)
+        unameElement=driver.find_element_by_id("userID")
+        passwordElement=driver.find_element_by_id("userPW")
+        time.sleep(1)
+        unameElement.click
+        time.sleep(2)
+        unameElement.send_keys(username)
+        time.sleep(2)
+        passwordElement.click()
+        time.sleep(2)
+        passwordElement.send_keys(password)
+        time.sleep(2)
+        #load captcha iframe
+        driver.switch_to_frame(driver.find_element_by_xpath("/html/body/div[3]/div[5]/div/div[2]/div[1]/form/table/tbody/tr[7]/td/div/div/div/iframe"))
+        #click captcha
+        time.sleep(2)
+        captchaClick=driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[1]/div/div/span/div[1]')
+        time.sleep(1)
+        captchaClick.click()
+        time.sleep(random.randrange(3,6))
+        #handle new iframe popup
+        driver.switch_to_default_content()
+        driver.switch_to_frame(driver.find_element_by_xpath("/html/body/div[5]/div[4]/iframe"))
+        time.sleep(5)
+        busterButton = driver.find_element_by_xpath('/html/body/div/div/div[3]/div[2]/div[1]/div[1]/div[4]')
+        busterButton.click()
+        time.sleep(10)
+        #leave captcha iframe
+        driver.switch_to_default_content()
+        time.sleep(1)
+        loginButton = driver.find_element_by_xpath('//*[@id="frmLogin"]/div/button')
+        loginButton.click()
+        time.sleep(10)
+    credentials.close()      
     
 except KeyboardInterrupt:
     print("Keyboard Intterupt, Exiting")
